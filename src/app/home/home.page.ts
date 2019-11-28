6a20139e82fbefe7bf79e456847b16be0aada9b4
aliyome <aliyome@gmail.com>
2019-11-29 03:26:36 +0900

    feat: Show wordpress article list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  }[] = [];
+  constructor(private readonly http: HttpClient) {}
+
+  ionViewDidEnter() {
+    this.http
+      .get(
+        'http://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/',
+      )
+      .subscribe(data => {
+        this.posts = data['posts'];
+      });
+  }
 }
