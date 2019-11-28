570b1f7c1b3440f85cc0933077631d693280b6d1
aliyome <aliyome@gmail.com>
2019-11-29 04:23:11 +0900

    feat: Send gtag when navigation ends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private readonly router: Router,
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,14 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(params => {
+      if (params instanceof NavigationEnd && !this.platform.is('hybrid')) {
+        gtag('config', 'UA-*****-**', { page_path: params.url });
+      }
     });
   }
 }
